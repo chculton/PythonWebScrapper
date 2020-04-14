a1701a9f7b0cc832bb07e635f9c0d6e3a8bc663b
C. Hans Culton <chansculton@gmail.com>
2020-04-14 19:16:24 +0930

    Trying lots of different solutions to get this to work

--- WebScrapper.py
+++ WebScrapper.py
@@ -3,10 +3,17 @@ from requests.exceptions import RequestException;
 from contextlib import closing;
 from bs4 import BeautifulSoup;
 
-from urllib.request import urlopen as uReq;
+from selenium import webdriver
+
+#from urllib.request import urlopen as uReq;
 
 # temporary, as this value will be read from a txt file
+
+# Probably cannot use stocktwits for now due to all the data I am interested
+# in is returned by ajax requests
 URL = "https://stocktwits.com/discover/earnings-calendar/";
+#URL = "https://stocktwits.com/discover/earnings-calendar/2020-04-14";
+#URL = "https://ql.stocktwits.com/batch?symbols=TSRI%2CARTW%2CLONE%2CXELB%2CLOAN%2CPNM%2CHIFS";
 
 def getURL(url):
     try:
@@ -29,20 +36,28 @@ def logError(errorResponse):
 #print(getURL(URL));
 #raw_html = open(getURL(URL)).read();
 #html = BeautifulSoup(getURL(URL), "html.parser");
-client = uReq(URL);
-soup = BeautifulSoup(client.read(), 'html.parser');
-
-#soup = BeautifulSoup(getURL(URL));
+#client = uReq(URL);
+#soup = BeautifulSoup(client.read(), 'html.parser');
+browser = webdriver.PhantomJS();
+browser.get(URL);
+html = browser.page_source;
+soup = BeautifulSoup(html, "lxml");
+#soup = BeautifulSoup(getURL(URL), "html.parser");
 #for p in html.select('p'):
 for span in soup.select('span'):
     #if(span['class'] == 'st_1QzH2P8 st_8u0ePN3'):
     try:
         #if span['class'] == "st_1QzH2P8 st_8u0ePN3":
-        if span['class'] == "st_8u0ePN3":
-            print(span.text);
-            print("Found span");
-        print(span["class"]);
-        print("Searching");
+        for data in span['class']:
+            if data == "st_8u0ePN3":
+                print(span.contents);
+                print("Found data!");
+            #print(data);
+        #if span['class'] == "st_8u0ePN3":
+            #print(span.text);
+            #print("Found span");
+        #print(span["class"]);
+        #print("Searching");
     #except:
     except(AttributeError, KeyError) as er:
         logError("Failed Except");
