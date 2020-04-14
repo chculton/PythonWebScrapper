7fa7d9ff99af3789736ddb514e9bdf7466ecf3f8
C. Hans Culton <chansculton@gmail.com>
2020-04-14 15:36:02 +0930

    Fixed an error in getURL, added an error logging function; html of inputted URL currently printed to console

--- WebScrapper.py
+++ WebScrapper.py
@@ -5,14 +5,20 @@ from bs4 import BeautifulSoup;
 
 def getURL(url):
     try:
-        with closing(get(url,stream=true)) as response:
+        with closing(get(url,stream=True)) as response:
             if goodResponse(response):
                 return response.content;
             else:
                 return None;
     except RequestException as Ex:
-        log_error("Error during requests to {0} : {1}".format(url, str(Ex)));
+        logError("Error during requests to {0} : {1}".format(url, str(Ex)));
 
 def goodResponse(_response):
     contentType = _response.headers["Content-Type"].lower();
     return (_response.status_code == 200 and contentType is not None and contentType.find("html") > -1);
+
+def logError(errorResponse):
+    print(errorResponse);
+
+URL = "https://stocktwits.com/discover/earnings-calendar/";
+print(getURL(URL));
