89aa4bc6821ca338a1559d1bc939681176a8986d
C. Hans Culton <chansculton@gmail.com>
2020-04-15 12:35:36 +0930

    Depreciating requests library and the code which used the library

--- WebScrapper.py
+++ WebScrapper.py
@@ -1,13 +1,14 @@
-from requests import get;
-from requests.exceptions import RequestException;
+# External Libraries
+#from requests import get;
+#from requests.exceptions import RequestException;
 from contextlib import closing;
 from bs4 import BeautifulSoup;
-
-#from selenium.webdriver import Firefox;
 from selenium import webdriver;
 
+# Python Libraries
 import os;
 
+# Local files
 import TxtParser;
 
 # Initialize storage for parsed file data
@@ -24,19 +25,20 @@ URL = textFileData[0];
 #URL = "https://stocktwits.com/discover/earnings-calendar/2020-04-14";
 #URL = "https://ql.stocktwits.com/batch?symbols=TSRI%2CARTW%2CLONE%2CXELB%2CLOAN%2CPNM%2CHIFS";
 
-def getURL(url):
-    try:
-        with closing(get(url,stream=True)) as response:
-            if goodResponse(response):
-                return response.content;
-            else:
-                return None;
-    except RequestException as Ex:
-        logError("Error during requests to {0} : {1}".format(url, str(Ex)));
+# Function Definitions
+#def getURL(url):
+#    try:
+#        with closing(get(url,stream=True)) as response:
+#            if goodResponse(response):
+#                return response.content;
+#            else:
+#                return None;
+#    except RequestException as Ex:
+#        logError("Error during requests to {0} : {1}".format(url, str(Ex)));
 
-def goodResponse(_response):
-    contentType = _response.headers["Content-Type"].lower();
-    return (_response.status_code == 200 and contentType is not None and contentType.find("html") > -1);
+#def goodResponse(_response):
+#    contentType = _response.headers["Content-Type"].lower();
+#    return (_response.status_code == 200 and contentType is not None and contentType.find("html") > -1);
 
 def logError(errorResponse):
     print(errorResponse);
@@ -46,21 +48,12 @@ def establishHeadlessFirefox():
      firefoxOptions.add_argument('--headless');
      return webdriver.Firefox(options=firefoxOptions, service_log_path=localDirectory+"\\geckodriver.log");
 
-#browser = webdriver.Firefox(service_log_path="C:\\Users\\chculton\\AppData\\Local\\Temp\\geckodriver.log");
-
-#browser = webdriver.Firefox(service_log_path="C:\\Users\\chans\\Documents\\Github\\PythonWebScrapper\\geckodriver.log");
+# MAIN
 browser = establishHeadlessFirefox();
-
-#browser.FirefoxOptions().add_arguement("--headless");
-
 browser.get(URL);
-#html = browser.page_source;
 soup = BeautifulSoup(browser.page_source, "html.parser");
 
-#soup = BeautifulSoup(getURL(URL), "html.parser");
-#for p in html.select('p'):
 for span in soup.select('span'):
-    #if(span['class'] == 'st_1QzH2P8 st_8u0ePN3'):
     try:
         #if span['class'] == "st_1QzH2P8 st_8u0ePN3":
         for data in span['class']:
@@ -69,11 +62,11 @@ for span in soup.select('span'):
                 print("Found data!");
             #print(data);
         #if span['class'] == "st_8u0ePN3":
-            #print(span.text);
+            #print(span.text);S
             #print("Found span");
         #print(span["class"]);
         #print("Searching");
-    #except:
-    except(AttributeError, KeyError) as er:
-        logError("Failed Except");
+    except:
+    #except(AttributeError, KeyError) as er:
+        logError("Span did not contain a class tag");
         pass;
