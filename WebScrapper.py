541f6b786eeb89f96f0d38edaf5fa3c6f471b61d
C. Hans Culton <chansculton@gmail.com>
2020-08-01 23:17:42 +0930

    Able to read all but the final ticker via window scrolling

--- WebScrapper.py
+++ WebScrapper.py
@@ -7,6 +7,7 @@ from selenium import webdriver;
 import numpy as np;
 
 from selenium.webdriver.support.ui import WebDriverWait;
+#from selenium.webdriver.support import expected_conditions as EC
 
 import time;
 
@@ -56,77 +57,108 @@ def establishHeadlessFirefox():
 
 # MAIN
 browser = establishHeadlessFirefox();
+#browser.implicitly_wait(10);
 browser.get(URL);
-#print (browser.page_source.encode("utf-8"));
-wait = WebDriverWait(browser, 10);
+#browser.implicitly_wait(10);
+print(browser.page_source.encode("utf-8"));
+#wait = WebDriverWait(browser, 10);
+#//element = wait.until(EC.presence_of_all_elements_located((By.ID, 'class')))
+
+
+
+
 # seems that there is a maximum amount of html that can be returned
 # parsing the python seems to parse the same amount of code as
 # previously, just now its the last 16 stocks, rather than the prior 17
 #time.sleep(5);
-soup = BeautifulSoup(browser.page_source, "html.parser");
-#print(soup)
-# Closes the browser
-#browser.quit();
+def findTickers():
+    soup = BeautifulSoup(browser.page_source, "html.parser");
+    #print(soup)
+    # Closes the browser
+    #browser.quit();
 
-#tickerStorage = [[],[]];
-#tickerDetailsStorage = [["first", "row"], ["Second", "row"]];
-#tickerDetailsStorage = np.array();
-ticker = 0;
-tickerDetails = 0;
-rowOfData = [];
+    #tickerStorage = [[],[]];
+    #tickerDetailsStorage = [["first", "row"], ["Second", "row"]];
+    #tickerDetailsStorage = np.array();
+    ticker = 0;
+    tickerDetails = 0;
+    rowOfData = [];
 
-isPositive = False;
-for span in soup.select('span'):
-    try:
-        #if span['class'] == "st_1QzH2P8 st_8u0ePN3":
-        for data in span['class']: #this essentially looks through all spans
-            if data == "st_w-QlNFW":
-                isPositive = True;
-                rowOfData.append(" Positive");
-            if data == "st_37VuZWc":
-                rowOfData.append(" Negative");
-            if data == "st_8u0ePN3":
-                #try:
-                #    for searchForColor in data.contents:
-                #        if searchForColor == "st_2fTou_q":
-                #            rowOfData.append("Positive");
-                #        else:
-                #            rowOfData.append("Negative");
-                #except:
-                #    print("Can't find color.");
-                # the content prints as ticker, price, point increase, then percentage increase
-                if span.text:
-                    rowOfData.append(span.text);
-                    #if(isPositive):
-                    #    rowOfData.append("Positive");
-                    #    isPositive = False;
-                    #else: rowOfData.append("Negative");
+    isPositive = False;
+    for span in soup.select('span'):
+        try:
+            #if span['class'] == "st_1QzH2P8 st_8u0ePN3":
+            for data in span['class']: #this essentially looks through all spans
+                if data == "st_w-QlNFW":
+                    isPositive = True;
+                    rowOfData.append(" Positive");
+                if data == "st_37VuZWc":
+                    rowOfData.append(" Negative");
+                if data == "st_8u0ePN3":
+                    #try:
+                    #    for searchForColor in data.contents:
+                    #        if searchForColor == "st_2fTou_q":
+                    #            rowOfData.append("Positive");
+                    #        else:
+                    #            rowOfData.append("Negative");
+                    #except:
+                    #    print("Can't find color.");
+                    # the content prints as ticker, price, point increase, then percentage increase
+                    if span.text:
+                        rowOfData.append(span.text);
+                        #if(isPositive):
+                        #    rowOfData.append("Positive");
+                        #    isPositive = False;
+                        #else: rowOfData.append("Negative");
 
-                tickerDetails += 1;
-        tickerDetails = 0;
-        ticker += 1;
-    except:
-        #logError("Span did not contain a class tag");
-        pass;
+                    tickerDetails += 1;
+            tickerDetails = 0;
+            ticker += 1;
+        except:
+            #logError("Span did not contain a class tag");
+            pass;
 
-dataToSave = np.array(rowOfData);
-index = [0];
-newArray = np.delete(dataToSave, index);
+    dataToSave = np.array(rowOfData);
+    index = [0];
+    newArray = np.delete(dataToSave, index);
 
-stockInfoString = "";
+    stockInfoString = "";
 
-stockInformation = [];
-for i in range(len(newArray)):
-    #if i%3 == 0:
-    if i%4 == 0 and i != 0:
-        stockInformation.append(stockInfoString);
-        stockInfoString = "";
-    tempString2 = newArray[i];
-    if(len(tempString2.split()) > 1):
-        newTempArray = tempString2.split();
-        stockInfoString += newTempArray[0] + " " + newTempArray[1];
-    else:
-        stockInfoString += tempString2 + " ";
+    stockInformation = [];
+    for i in range(len(newArray)):
+        #if i%3 == 0:
+        if i%4 == 0 and i != 0:
+            stockInformation.append(stockInfoString);
+            stockInfoString = "";
+        tempString2 = newArray[i];
+        if(len(tempString2.split()) > 1):
+            newTempArray = tempString2.split();
+            stockInfoString += newTempArray[0] + " " + newTempArray[1];
+        else:
+            stockInfoString += tempString2 + " ";
 
-for k in range(len(stockInformation)):
-    print(str(k) + " entry in stock info: " + stockInformation[k]);
+    for k in range(len(stockInformation)):
+        print(str(k) + " entry in stock info: " + stockInformation[k]);
+
+def scrollPage(_xScrollAmountInPixels, _yScrollAmountInPixels):
+    browserCommand = "window.scrollBy(" + str(_xScrollAmountInPixels) + "," + str(_yScrollAmountInPixels) + ")"
+    for i in range(2):
+        browser.execute_script(browserCommand)
+        findTickers()
+        time.sleep(2)
+
+# Scroll down the page
+def scrollToPageBottom():
+    currentPage = browser.page_source
+    for x in range(20):
+        scrollPage(0, 500)
+        scrolledPage = browser.page_source
+        if scrolledPage != currentPage:
+            currentPage = scrolledPage
+        else:
+            # if this happens 2 in a row, page bottom had been met
+            # close the browser window
+            break
+
+#currentPage = browser.page_source
+scrollToPageBottom();
